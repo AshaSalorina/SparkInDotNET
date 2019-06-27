31a9fd1166b64776cba5169773b93f72c628161c
AshaSalorina <he670004272@gmail.com>
2019-06-27 19:19:05 +0900

    提交一份过滤清单，移动文件位置，开始准备sparksql的测试

---
+++ CoreSite/testDir/SparkSQL/SparkSqlForSqlServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Spark;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace CoreSite.testDir.SparkSQL
+{
+    public class SparkSqlForSqlServer
+    {
+        public static async Task ShowData(SparkSession sparkSession)
+        {
+            //var spsql = sparkSession.Read().s
+        }
+    }
+}
